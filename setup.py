abbdb81a66f5e0e0fda49b85e9358ab9f6d3fc2d
opencm <limor@gigaspaces.com>
2014-11-10 12:32:51 +0200

    Bump version to 1.1rc2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-libcloud-provider',
-    version='1.1rc1',
+    version='1.1rc2',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify_libcloud'],
@@ -30,6 +30,6 @@ setup(
     install_requires=[
         'apache-libcloud==0.15.1',
         'IPy==0.81',
-        'cloudify==3.1rc1'
+        'cloudify==3.1rc2'
     ]
 )
